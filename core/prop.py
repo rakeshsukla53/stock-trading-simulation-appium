31b55fc779bd374dcfc81543dfb0f5b20daf36b7
Rakesh Sukla <rakesh.sukla53@gmail.com>
2022-04-28 14:22:38 -0400

    android apk absolute path

--- core/prop.py
+++ core/prop.py
@@ -1,8 +1,23 @@
+from pathlib import Path
+
+appium_webdriver_host = "http://localhost:4723/wd/hub"
+webdriver_start_retry = 5
+implicitly_wait_time = 6
+explicit_wait_time = 5
+
+
+def get_current_working_directory():
+    parent_folder_path = Path(__file__).resolve().parent
+    project_path = parent_folder_path.parent
+    android_apk_path = "/apk/stock-simulator.apk"
+    android_absolute_path = str(project_path) + android_apk_path
+    return android_absolute_path
+
 
 android_app = [
 
     {
-        "app": "/Users/rakeshsukla/Downloads/Simulator_v1.1.5_apkfab.com.apk",
+        "app": get_current_working_directory(),
         "deviceName": "pixel_2",
         "platformName": "Android",
         "automationName": "uiautomator2",
@@ -12,8 +27,3 @@ android_app = [
     }
 
 ]
-
-appium_webdriver_host = "http://localhost:4723/wd/hub"
-webdriver_start_retry = 5
-implicitly_wait_time = 6
-explicit_wait_time = 5
